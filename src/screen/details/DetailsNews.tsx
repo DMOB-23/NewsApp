d5aa951e9b66f239de9fcdec032ed49ccf880dfe
Roberto Padilha <robertocesardasilva@gmail.com>
2023-04-10 19:20:40 -0300

    Aula 10/04

--- src/screen/details/DetailsNews.tsx
+++ src/screen/details/DetailsNews.tsx
@@ -1,8 +1,19 @@
-import { View } from "react-native"
+import { Button, Image, ScrollView, Text, View } from "react-native"
+import styles from "./Styles"
 
 const DetailsNews = () => {
     return (
-        <View></View>
+        <ScrollView contentContainerStyle={styles.container}>
+            <View style={styles.container}>
+                <Image source={{  }} style={styles.image} />
+                
+                <Text style={styles.title}>title</Text>
+                <Text style={styles.source}>source</Text>
+                <Text style={styles.content}>content</Text>
+                
+                <Button onPress={() => null} title="Continue lendo"/>
+            </View>
+        </ScrollView>
     )
 }
 
